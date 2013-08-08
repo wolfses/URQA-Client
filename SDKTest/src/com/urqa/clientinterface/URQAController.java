6b24aee6433ad16808fc9468ec9bf11156b3a8e6
wolfses <wolfses@hotmail.com>
2013-08-09 01:22:55 +0900

    데이터 얻어오기 완성

---
+++ SDKTest/src/com/urqa/clientinterface/URQAController.java
@@ -0,0 +1,68 @@
+package com.urqa.clientinterface;
+
+import com.urqa.rank.ErrorRank;
+
+import android.content.Context;
+
+public final class URQAController {
+
+	public static boolean Initialize(Context context, String APIKEY)
+	{
+		
+		return false;
+	}
+	
+	public static void SendException(Exception e, String Tag, ErrorRank rank)
+	{
+		
+	}
+	
+	public static void SetLogCat(boolean toggleLog)
+	{
+		
+	}
+
+	public static void SetLogging(int Line, String Filter)
+	{
+		
+	}
+	
+	public static void StartActivity(Context context)
+	{
+		
+	}
+	
+	public static void EndActivity(Context context)
+	{
+		
+	}
+	
+	
+	/*
+	public class Log{
+				
+		public static int i(String tag, String Msg, Throwable tr)
+		{
+		}
+		public static  int i(String tag, String Msg)
+		{}
+		public static  int d(String tag, String Msg, Throwable tr)
+		{}
+		public static  int d(String tag, String Msg)
+		{}
+		public static  int w(String tag, String Msg, Throwable tr)
+		{}
+		public static  int w(String tag, String Msg)
+		{}
+		public static  int e(String tag, String Msg, Throwable tr)
+		{}
+		public static  int e(String tag, String Msg)
+		{}
+		public static  int f(String tag, String Msg, Throwable tr)
+		{}
+		public static  int f(String tag, String Msg)
+		{}
+	}
+	*/
+	
+}
---
+++ SDKTest/src/com/urqa/rank/ErrorRank.java
@@ -0,0 +1,5 @@
+package com.urqa.rank;
+
+public enum ErrorRank {
+	Unhandle,Critical,Major,Minor
+}
