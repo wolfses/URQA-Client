9bfe0ef9112db4551fe5a5fc94c43688d7f40a71
wolfses <wolfses@hotmail.com>
2013-08-13 11:20:16 +0900

    unhandle 완성

--- SDKTest/src/com/urqa/clientinterface/URQAController.java
+++ SDKTest/src/com/urqa/clientinterface/URQAController.java
@@ -17,6 +17,8 @@ import com.google.gson.Gson;
 import com.urqa.Collector.CallStackCollector;
 import com.urqa.Collector.DateCollector;
 import com.urqa.Collector.DeviceCollector;
+import com.urqa.Collector.ErrorReport;
+import com.urqa.Collector.ErrorReportFactory;
 import com.urqa.Collector.LogCollector;
 import com.urqa.common.CallStackData;
 import com.urqa.common.Network;
@@ -107,68 +109,17 @@ public final class URQAController {
 		SendEventPath.SetNetwork(StateData.ServerAddress + "client/connect", 
 								 gson.toJson(SendEventdata), 
 								 Network.Networkformula.POST);
-		
 	}
 
-	private static ErrorSendData CreateSendData(Exception e,String tag,ErrorRank rank ,Context context)
-	{
-		ErrorSendData senddata = new ErrorSendData();
-		
-		String CallStack = CallStackCollector.GetCallStack(e);
-		CallStackData data = CallStackCollector.ParseStackTrace(e, CallStack);
-		
-		PackageManager packagemanager = context.getPackageManager();
-		PackageInfo packageinfo = null;
-		try {
-			packageinfo = packagemanager.getPackageInfo(context.getPackageName(), 0);
-		} catch (NameNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			return null;
-		}
-		
-		senddata.apikey 		= StateData.APIKEY;
-		senddata.datetime 		= DateCollector.GetDateYYMMDDHHMMSS(context);
-		senddata.device			= android.os.Build.MODEL;
-		senddata.country		= DeviceCollector.GetNational(context);
-		senddata.errorname		= data.ErrorName; 
-		senddata.errorclassname	= data.ClassName;
-		senddata.linenum		= data.Line;
-		senddata.callstack		= CallStack;
-		senddata.appversion		= packageinfo.versionName;
-		senddata.osversion		= android.os.Build.VERSION.RELEASE;
-		senddata.gpson			= (DeviceCollector.GetGps(context)) ? 1 : 0;
-		senddata.wifion			= (DeviceCollector.GetWiFiNetwork(context)) ? 1 : 0;
-		senddata.mobileon		= (DeviceCollector.GetMobileNetwork(context)) ? 1 : 0;
-		senddata.scrwidth		= DeviceCollector.GetWidthScreenSize(context);
-		senddata.scrheight		= DeviceCollector.GetHeightScreenSize(context);
-		senddata.batterylevel	= DeviceCollector.GetBatteryLevel(context);
-		senddata.availsdcard	= DeviceCollector.BytetoMegaByte(DeviceCollector.getAvailableExternalMemorySize());
-		senddata.rooted			= (DeviceCollector.CheckRoot()) ? 1 : 0;
-		senddata.appmemtotal	= DeviceCollector.BytetoMegaByte(DeviceCollector.GetTotalMemory());
-		senddata.appmemfree		= DeviceCollector.BytetoMegaByte(DeviceCollector.GetFreeMemory());
-		senddata.appmemmax		= DeviceCollector.BytetoMegaByte(DeviceCollector.GetMaxMemory());
-		senddata.kernelversion	= DeviceCollector.GetLinuxKernelVersion();
-		senddata.xdpi			= DeviceCollector.GetXDPI(context);
-		senddata.ydpi			= DeviceCollector.GetYDPI(context);
-		senddata.scrorientation = DeviceCollector.GetOrientation(context);
-		senddata.sysmemlow		= (DeviceCollector.GetSystemLowMemory()) ? 1 : 0;
-		senddata.tag			= tag;
-		senddata.rank			= rank.value();
-		senddata.eventpaths		= EventPathManager.GetErrorEventPath();
-		senddata.locale			= DeviceCollector.GetLocale(context);
-		
-		return senddata;
-	}
+
 	public static void SendException(Exception e, String Tag, ErrorRank rank)
 	{
-		ErrorSendData sendData = CreateSendData(e,Tag,rank,StateData.AppContext);
-		String Log		= LogCollector.getLog(StateData.AppContext);
+		ErrorReport report = ErrorReportFactory.CreateErrorReport(e, Tag, rank, StateData.AppContext);
 		
-		SendErrorProcess errprocess = new SendErrorProcess(sendData,Log);
+		SendErrorProcess errprocess = new SendErrorProcess(report, "client/send/exception");
 		errprocess.start();
 	}
-	public static void SendException(Exception e)
+	public static void SendException(Exception e)	
 	{
 		SendException(e, null, ErrorRank.Nothing);
 	}
