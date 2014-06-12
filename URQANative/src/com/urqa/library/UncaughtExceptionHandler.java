a7b56426343e2aeef526852a142db2518a6c3091
SeunOh <zeros0317@gmail.com>
2014-06-13 06:11:31 +0900

    앱 락 문제 수정

--- URQANative/src/com/urqa/library/UncaughtExceptionHandler.java
+++ URQANative/src/com/urqa/library/UncaughtExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.urqa.library;
 
+import android.util.Log;
+
 import com.urqa.Collector.ErrorReport;
 import com.urqa.Collector.ErrorReportFactory;
 import com.urqa.common.SendErrorProcess;
@@ -9,29 +11,37 @@ import com.urqa.rank.ErrorRank;
 public class UncaughtExceptionHandler implements
 		Thread.UncaughtExceptionHandler {
 
-	private Thread.UncaughtExceptionHandler  mUncaughtExceptionHandler = null;
-	
+	private Thread.UncaughtExceptionHandler mDefaultExceptionHandler = null;
+	private Thread.UncaughtExceptionHandler mUncaughtExceptionHandler = null;
+
 	public UncaughtExceptionHandler() {
+		mDefaultExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+
 		Thread.setDefaultUncaughtExceptionHandler(this);
 	}
 
-	public UncaughtExceptionHandler(Thread.UncaughtExceptionHandler handler) {
-		mUncaughtExceptionHandler = handler;
+	public void setUncaughtExceptionHandler(
+			Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
+		mUncaughtExceptionHandler = uncaughtExceptionHandler;
 
 	}
-	
-	
+
 	@Override
 	public void uncaughtException(Thread thread, Throwable ex) {
-		if (mUncaughtExceptionHandler != null)
-			mUncaughtExceptionHandler.uncaughtException(thread, ex);
-		
-		
-		ErrorReport report = ErrorReportFactory.CreateErrorReport(ex, "",
-				ErrorRank.Unhandle, StateData.AppContext);
+		try {
+			ErrorReport report = ErrorReportFactory.CreateErrorReport(ex, "",
+					ErrorRank.Unhandle, StateData.AppContext);
 
-		SendErrorProcess process = new SendErrorProcess(report);
-		process.start();
+			SendErrorProcess process = new SendErrorProcess(report);
+			process.start();
+
+			if (mUncaughtExceptionHandler != null)
+				mUncaughtExceptionHandler.uncaughtException(thread, ex);
+		} catch (Throwable e) {
+			Log.e("UrQA", e.getMessage());
+		} finally {
+			mDefaultExceptionHandler.uncaughtException(thread, ex);
+
+		}
 	}
-
 }
